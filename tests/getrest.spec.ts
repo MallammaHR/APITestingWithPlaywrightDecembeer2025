6922c9b09831e2bfb681247ec8e54a711c226a7d
MallammaHR <mallumanish17@gmail.com>
2025-12-17 16:11:46 -0600

    adding only POST-call

--- tests/getrest.spec.ts
+++ tests/getrest.spec.ts
@@ -1,27 +1,12 @@
 import { test, expect, request } from '@playwright/test';
 
-const TOKEN = '4d1a5b3f15cb1b87d9be3412b697c0a3ca30af72bb8ab33ea7591f8e1ae07f8a';
+const TOKEN = '4a3c982bab4c3be6047e6007c6c349f071076fd5777631e14ebab09c287f1dd5';
 
 
 //creating new user
-// test('GET - fetch all users', async ({ request }) => {
-
-//   const response = await request.get('https://gorest.co.in/public/v2/users', {
-//     headers: {
-//       Authorization: `Bearer ${TOKEN}`
-//     }
-//   });
-
-//   expect(response.status()).toBe(200);
-
-//   const data = await response.json();
-//   console.log(data);
-// });
-
-//Get single User Details
 test('GET - fetch all users', async ({ request }) => {
 
-  const response = await request.get('https://gorest.co.in/public/v2/users/8229210', {
+  const response = await request.get('https://gorest.co.in/public/v2/users', {
     headers: {
       Authorization: `Bearer ${TOKEN}`
     }
@@ -33,27 +18,42 @@ test('GET - fetch all users', async ({ request }) => {
   console.log(data);
 });
 
+// //Get single User Details
+// test('GET - fetch all users', async ({ request }) => {
 
-test('POST_create a user',async({request}) => {
+//   const response = await request.get('https://gorest.co.in/public/v2/users/8229210', {
+//     headers: {
+//       Authorization: `Bearer ${TOKEN}`
+//     }
+//   });
 
-const requestBody = {
+//   expect(response.status()).toBe(200);
+
+//   const data = await response.json();
+//   console.log(data);
+// });
+
+
+// test('POST_create a user',async({request}) => {
+
+// const requestBody = {
  
-        name:'PW Test User',
-        email :`pwtest${Date.now()}@mail.com`,
-        gender  :'female',
-        status :'active'
+//         name:'PW Test User',
+//         email :`pwtest${Date.now()}@mail.com`,
+//         gender  :'female',
+//         status :'active'
 
-    };
-const response = await request.post('https://gorest.co.in/public/v2/users/' ,{
-    headers : {
-        Authorization : `Bearer ${TOKEN}`,
-    },
+//     };
+// const response = await request.post('https://gorest.co.in/public/v2/users/' ,{
+//     headers : {
+//         Authorization : `Bearer ${TOKEN}`,
+//     },
 
-    data:requestBody
-});
-expect( response.status()).toBe(201);
-const data = await response.json();
-console.log(data);
+//     data:requestBody
+// });
+// expect( response.status()).toBe(201);
+// const data = await response.json();
+// console.log(data);
 
 
-});
+// });
